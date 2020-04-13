64d87969f86a230296dec706948fe164bcf4f504
ralva <robert.alva@tranzact.net>
2020-04-13 00:58:59 -0500

    Removed Unused events

--- Breaker/Events/CircuitBreakerEvents.cs
+++ Breaker/Events/CircuitBreakerEvents.cs
@@ -6,21 +6,6 @@ namespace Breaker.Events
     public class CircuitBreakerEvents
     {
         /// <summary>
-        /// Raised when the circuit breaker enters the closed state
-        /// </summary>
-        public event Action<CircuitBreakerOpened> ClosedCircuitBreaker;
-
-        /// <summary>
-        /// Raised when the circuit breaker enters the opened state
-        /// </summary>
-        public event Action<CircuitBreakerOpened> OpenedCircuitBreaker;
-
-        /// <summary>
-        /// Raised when the circuit breaker enters the Half open state
-        /// </summary>
-        public event Action<CircuitBreakerHalfOpen> HalfOpenedCircuitBreaker;
-
-        /// <summary>
         /// Raised when the circuit breaker changes state
         /// </summary>
         public event Action<CircuitBreakerState> OnStateChange;
